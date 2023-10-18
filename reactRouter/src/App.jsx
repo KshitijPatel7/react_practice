3cf2639b8a2f3809e52f310007a69f1d6560eb27
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-18 22:18:46 +0530

    completed header,jsx

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='bg-purple-600 p-4'>React router</h1>
+    </>
+  )
+}
+
+export default App
