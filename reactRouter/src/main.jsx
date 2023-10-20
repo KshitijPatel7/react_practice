739fa3e459c9f9bf8af1ca2eb20cc3334dbafd35
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-20 18:16:53 +0530

    updated main.jsx

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    childern: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider  router={router} />
   </React.StrictMode>,
 )
