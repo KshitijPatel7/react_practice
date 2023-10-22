c556183ba45a4adf45f659127ee7f3f4afc904d4
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-22 21:39:04 +0530

    updated main.jsx

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -7,6 +7,7 @@ import Layout from './Layout.jsx'
 import Home from './components/home/Home.jsx'
 import About from './components/about/About.jsx'
 import Contact from './components/contact/Contact.jsx'
+import Github, {githubInfoLoader} from './components/github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -36,7 +37,7 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
-      <Route path='github' element={<Github />} />
+      <Route loader={githubInfoLoader} path='github' element={<Github />} />
     </Route> 
   )
 )
