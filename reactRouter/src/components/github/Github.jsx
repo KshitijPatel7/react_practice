379e7441f8e494841909cd2aad187f4a5f3989cf
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-22 21:26:55 +0530

    updated github.jsx

--- reactRouter/src/components/github/Github.jsx
+++ reactRouter/src/components/github/Github.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Github = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch('https://api.github.com/users/kshitijpatel')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setData(data);
+    })
+  }, [])
+
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers:</div>
   )
