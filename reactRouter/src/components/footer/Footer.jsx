6ac27694f225ae64d8ee75d07a2c4f58a0783fb6
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-26 20:01:22 +0530

    Update Footer.jsx

--- reactRouter/src/components/footer/Footer.jsx
+++ reactRouter/src/components/footer/Footer.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer className="bg-white border-y">
-            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-                <div className="md:flex md:justify-between">
-                    <div className="mb-6 md:mb-0">
-                        <Link to="/" className="flex items-center">
-                            <img
-                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                                className="mr-3 h-16"
-                                alt="Logo"
-                            />
-                        </Link>
+        // <footer className="bg-white border-y">
+        //     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        //         <div className="md:flex md:justify-between">
+        //             <div className="mb-6 md:mb-0">
+        //                 <Link to="/" className="flex items-center">
+        //                     <img
+        //                         src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+        //                         className="mr-3 h-16"
+        //                         alt="Logo"
+        //                     />
+        //                 </Link>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
