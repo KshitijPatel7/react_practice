30d9375223fd757c196d699cffc2b4a8ea516fe4
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-27 20:42:32 +0530

    Update Github.jsx

--- reactRouter/src/components/github/Github.jsx
+++ reactRouter/src/components/github/Github.jsx
@@ -13,12 +13,12 @@ const Github = () => {
     })
   }, [])
 
-  // return (
-  //   <>
-  //   <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}</div>
-  //   <img src={data.avatar_url} alt="Git picture" width={300} />
-  //   </>
-  // )
+  return (
+    <>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}</div>
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </>
+  )
 }
 
 export default Github
