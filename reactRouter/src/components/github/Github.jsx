2ac015225708b8923e0bcea3ea8f88b5e11a1be8
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-27 20:41:02 +0530

    Update Github.jsx

--- reactRouter/src/components/github/Github.jsx
+++ reactRouter/src/components/github/Github.jsx
@@ -2,23 +2,23 @@ import React, { useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 const Github = () => {
-  const data = useLoaderData()
-  // const [data, setData] = useState([])
-  // useEffect(() => {
-  //   fetch('https://api.github.com/users/kshitijpatel7')
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     console.log(data);
-  //     setData(data);
-  //   })
-  // }, [])
+  const dataa = useLoaderData()
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch('https://api.github.com/users/kshitijpatel7')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setData(data);
+    })
+  }, [])
 
   return (
-    <>
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}</div>
-    <img src={data.avatar_url} alt="Git picture" width={300} />
-    </>
-  )
+  //   <>
+  //   <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}</div>
+  //   <img src={data.avatar_url} alt="Git picture" width={300} />
+  //   </>
+  // )
 }
 
 export default Github
