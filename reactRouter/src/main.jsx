ef1523f41cf64588ed48155ae3e53a5b625d1802
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-29 19:28:51 +0530

    Update main.jsx

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -9,38 +9,38 @@ import About from './components/about/About.jsx'
 import Contact from './components/contact/Contact.jsx'
 import Github, {githubInfoLoader} from './components/github/Github.jsx'
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Layout />,
-//     childern: [
-//       {
-//         path: "",
-//         element: <Home />
-//       },
-//       {
-//         path: "about",
-//         element: <About />
-//       }
-//       {
-//         path: "contact",
-//         element: <Contact />
-//       }
-//     ]
-//   }
-// ])
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    childern: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='user/:userid' element={<User />} />
-      <Route loader={githubInfoLoader} path='github' element={<Github />} />
-    </Route> 
-  )
-)
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout />}>
+//       <Route path='' element={<Home />} />
+//       <Route path='about' element={<About />} />
+//       <Route path='contact' element={<Contact />} />
+//       <Route path='user/:userid' element={<User />} />
+//       <Route loader={githubInfoLoader} path='github' element={<Github />} />
+//     </Route> 
+//   )
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
