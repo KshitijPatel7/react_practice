2c29ea8bc63ca457e686b8462aa43d5b56c23514
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-30 11:15:56 +0530

    added login.jsx

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider'
 import './App.css'
 
 function App() {
  
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
